901e52b0f2211af1b2742b575992582ef7cbdbf8
realsubodhsingh <subodhsingh0021@gmail..com>
2024-10-10 03:25:09 +0530

    resolution

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
 
 function App() {
-<<<<<<< HEAD
   console.log(import.meta.env.VITE_APPWRITE_URL)
-
-  return (
-    <>
-      <h1>Setting up the Project</h1>
-        
-=======
-
+      
   return (
     <>
      <h1>Starting the mega project</h1>
->>>>>>> cdc522f (setting up the initial dependencies)
     </>
   )
 }
