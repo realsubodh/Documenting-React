16e277ce7246fd27c792c38c83bde8aa173cd61e
realsubodhsingh <subodhsingh0021@gmail..com>
2024-10-10 04:27:10 +0530

    deploying the setup

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,20 @@
+/* this is the production grade approach of loading the env components
+because sometimes it can happen that env entities failed to load */
+
+
+const conf ={
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE__PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+
+
+
+
+
+
+export default conf
